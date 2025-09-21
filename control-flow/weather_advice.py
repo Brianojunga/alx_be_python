b772b29181957c9127d2a50597373e94da15fa26
Brianojunga <brianojunga3@gmail.com>
2025-09-21 22:12:19 +0300

    Add weather advice code

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,9 @@
+wear = input("What's the weather like today? (sunny/rainy/cold): ")
+if wear == "sunny":
+    print('Wear a t-shirt and sunglasses.')
+elif wear == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif wear == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
