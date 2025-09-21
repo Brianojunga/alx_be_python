0eacdc84062f32d61c0f7d23179975d485f9d435
Brianojunga <brianojunga3@gmail.com>
2025-09-21 22:31:40 +0300

    Add the division errors

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -13,7 +13,7 @@ match operation:
         if num2 != 0:
             result = num1 / num2
         else:
-            result = "Error: Division by zero"
+            result = "Cannot divide by zero."
     case _:
         result = "Error: Invalid operation"
 
