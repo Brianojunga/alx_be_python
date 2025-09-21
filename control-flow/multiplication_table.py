34bd230051b3f5ad598c4cd7766c5bdfa2fee7eb
Brianojunga <brianojunga3@gmail.com>
2025-09-21 22:40:40 +0300

    Change the sign to *

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,4 @@ number = int(input("Enter a number to see its multiplication table: "))
 
 for i in range(1, 11):
     Z = number * i
-    print(f"{number} x {i} = {Z}")
+    print(f"{number} * {i} = {Z}")
