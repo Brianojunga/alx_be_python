f0d5a579f6e6787db92ad177e4c6634a2eaa7b4c
Brianojunga <brianojunga3@gmail.com>
2025-09-28 22:13:05 +0300

    Change the print to uses string format

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -13,7 +13,8 @@ def display_current_datetime():
 def calculate_future_date():
     number_of_days = int(input('Enter the number of days to add to the current date: '))
     future_date = datetime.now() + timedelta(days=number_of_days)
-    print(f'Future date: {future_date.date()}')
+    print(f'Future date: {future_date.strftime("%Y-%m-%d")}')
+
 
 
 if __name__ == "__main__":
